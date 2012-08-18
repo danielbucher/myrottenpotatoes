5755103aba745621894c1d4ce8a5faee7a2f9cea
DuffBeer88 <daniel.bucher88@gmail.com>
2012-08-18 17:02:19 -0300

    Completed topic 6.3

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -3,11 +3,12 @@ require 'spec_helper'
 describe MoviesController do
   describe 'searching TMDb' do
     it 'should call the model method that performs TMDb search' do
-      pending 'Need to choose name for model method.'
+      fake_results = [mock('movie1'), mock('movie2')]
+      Movie.should_receive(:find_in_tmdb).with('hardware').
+        and_return(fake_results)
+      post :search_tmdb, {:search_terms => 'hardware'}
     end
-    it 'should select the Search Results template for rendering' do
-      flunk 'No template exists yet.'
-    end
+    it 'should select the Search Results template for rendering'
     it 'should make the TMDb search results available to that template'
   end
 end

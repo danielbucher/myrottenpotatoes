adad35aaeb2ad62bf6f9b79fdea5110e3aae4335
DuffBeer88 <daniel.bucher88@gmail.com>
2012-08-20 00:42:59 -0300

    Finished topic 6.6

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,14 @@
 class Movie < ActiveRecord::Base
-  
+
+  class Movie::InvalidKeyError < StandardError ; end
+
+  def self.find_in_tmdb(string)
+    begin
+      TmdbMovie.find(:title => string)
+    rescue ArgumentError => tmdb_error
+      raise Movie::InvalidKeyError, tmdb_error.message
+    end      
+  end
+
+  # rest of file elided for brevity
 end
